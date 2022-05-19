ab5eace704fca0975885207cb939aa481f6b3a83
Joshua Shane Martin <josmar52789@gmail.com>
2022-05-19 18:29:59 -0400

    Adding additional barcode formats and bumping version.

--- BRIZBEE/ScanBarCodeViewController.swift
+++ BRIZBEE/ScanBarCodeViewController.swift
@@ -88,7 +88,12 @@ class ScanBarCodeViewController: UIViewController, AVCaptureMetadataOutputObject
                 
                 // Set delegate and use the default dispatch queue to execute the call back
                 captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-                captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.code128]
+                captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.code128, AVMetadataObject.ObjectType.code39,
+                                                             AVMetadataObject.ObjectType.code93, AVMetadataObject.ObjectType.code39Mod43,
+                                                             AVMetadataObject.ObjectType.upce, AVMetadataObject.ObjectType.ean8,
+                                                             AVMetadataObject.ObjectType.ean13, AVMetadataObject.ObjectType.interleaved2of5,
+                                                             AVMetadataObject.ObjectType.itf14, AVMetadataObject.ObjectType.pdf417,
+                                                             AVMetadataObject.ObjectType.aztec, AVMetadataObject.ObjectType.qr]
             } else {
                 return
             }
